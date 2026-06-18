eb044915ebc8a6813b45d5c8a7a9318d04fdb8c0
Edgar Fedora <edgarivancs@gmail.com>
2026-06-18 13:36:11 -0600

    fix: Error in card height is fixed

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,15 +1,14 @@
 import { animate } from "animejs";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useLayoutEffect, useState } from "react";
 
 import "./Stack.css";
 import { Snapshot, Frame } from "../../engine/types";
 
 interface StackProps {
-  snapshot: Snapshot | null,
-  renderCardItem: any,
-  stackHeight: number,
-  gap: number
-
+  snapshot: Snapshot | null;
+  renderCardItem: any;
+  stackHeight: number;
+  gap: number;
 }
 export default function Stack({
   snapshot,
@@ -18,16 +17,21 @@ export default function Stack({
   gap = 10,
 }: StackProps) {
   const refs = useRef(new Map());
+  const elementRef = useRef(null);
+  const [height, setHeight] = useState(0);
 
   const STACK_PADDING = 10;
 
-  const availableHeight = stackHeight - STACK_PADDING * 2;
-
-  const items:Frame[] = snapshot?.stack || [];
+  const items: Frame[] = snapshot?.stack || [];
+  const availableHeight = height - STACK_PADDING * 2;
+  console.log("height:", height);
 
   const cardHeight =
     items.length > 0
-      ? Math.max(50, (availableHeight - gap * (items.length - 1)) / items.length)
+      ? Math.max(
+          50,
+          (availableHeight - gap * (items.length - 1)) / items.length,
+        )
       : stackHeight;
 
   useEffect(() => {
@@ -45,8 +49,22 @@ export default function Stack({
     });
   }, [items, cardHeight, gap]);
 
+  useLayoutEffect(() => {
+    if (elementRef.current) {
+      const timer = setTimeout(() => {
+       setHeight(elementRef.current.offsetHeight);
+      }, 100); 
+
+      return () => clearTimeout(timer);
+    }
+  }, []); 
+
   return (
-    <div className="flex-1 relative border-1 border-slate-300 rounded-lg bg-white"> {/*flex-1 need to grow up automatically the stack*/}
+    <div
+      ref={elementRef}
+      className="stack relative flex-1 border-1 border-slate-300 rounded-lg bg-white"
+    >
+      {/*flex-1 need to grow up automatically the stack*/}
       {items.map((item, index) => (
         <div
           key={index}
